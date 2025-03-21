7e32cd78d1edbba85374d60ae548ee2b9a3c78ed
Daung <daung@Daungs-MacBook-Air.local>
2025-03-21 19:50:07 +0630

    font, logo  and template dir

--- sum_school/sum_app/views.py
+++ sum_school/sum_app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 def welcome(request):
     """Welcome view for the LMS homepage"""
     return render(request, 'welcome.html', {
-        'title': 'WARMLY WELCOME',
+        'title': 'SUM',
+        'page_title': 'WARMLY WELCOME TO SUM',
         'welcome_message': 'Welcome to Spring University Myanmar'
     })
