d8b8d72b442e1a020a6d27ee0276db2697235d48
zayminkhant <mzay5175@gmail.com>
2025-05-16 12:25:39 +0630

    classes Schedule

--- sum_school/sum_school/urls.py
+++ sum_school/sum_school/urls.py
@@ -37,5 +37,6 @@ urlpatterns = [
     path('dashboard/<str:course_id>/assignment/' , main_views.assignmentRedirect),
     path('dashboard/<str:course_id>/assignment/<int:m>' , main_views.assignment , name='assignment'),
     path('dashboard/<str:course_id>/assignment/<int:m>/<str:assignment_id>' , main_views.assignmentDetails , name='assignment_details'),
-    path('dashboard/<str:course_id>/people' , main_views.people , name='people')
+    path('dashboard/<str:course_id>/people' , main_views.people , name='people'),
+    path('dashboard/<str:course_id>/classes' , main_views.classes , name='classes'),
 ]
