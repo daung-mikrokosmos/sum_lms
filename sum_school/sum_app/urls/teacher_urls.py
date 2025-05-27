eec3a2cacf8453976c89669f0fa141e5f21c0c2c
Daung <daung@Daungs-MacBook-Air.local>
2025-05-27 23:07:01 +0630

    teacher

--- sum_school/sum_app/urls/teacher_urls.py
+++ sum_school/sum_app/urls/teacher_urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from sum_app.views import teacher_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 app_name = 'sum_teacher'
 
@@ -8,4 +11,23 @@ urlpatterns = [
     path('dashboard/', teacher_views.teacher_dashboard, name='teacher_dashboard'),
     path('login/authenticate/', teacher_views.teacher_login, name='teacher_login'),
     path('logout/', teacher_views.teacher_logout, name='logout'),
-]
+    path('my-profile/', teacher_views.teacher_profile, name='teacher_profile'),
+    
+    path('programs/<int:program_id>/dashboard/', teacher_views.program_dashboard, name='program_dashboard'),
+    path('programs/<int:program_id>/classes/', teacher_views.program_classes, name='program_classes'),
+    path('programs/<int:program_id>/classes/<int:class_id>/rolecalls/', teacher_views.show_rolecalls, name='show_rolecalls'),
+    path('programs/<int:program_id>/activities', teacher_views.program_activities, name='program_activities'),
+    path('programs/<int:program_id>/activities/create/', teacher_views.show_create_activity, name='show_create_activity'),
+    path('programs/<int:program_id>/activities/create/submit/', teacher_views.create_activity, name='create_activity'),
+    
+    path('programs/<int:program_id>/people', teacher_views.program_users, name='program_users'),
+    
+    path('programs/<int:program_id>/assignments', teacher_views.show_assignments, name='show_assignments'),
+    path('programs/<int:program_id>/assignments/create/', teacher_views.show_create_assignment, name='show_create_assignment'),
+    path('programs/<int:program_id>/assignments/create/submit/', teacher_views.create_assignment, name='create_assignment'),
+    path('programs/<int:program_id>/assignments/<int:task_id>/details/', teacher_views.show_assignment_details, name='show_assignment_details'),
+    
+    path('programs/<int:program_id>/my-profile', teacher_views.program_profile, name='program_profile'),
+    path('programs/<int:program_id>/my-profile/edit/', teacher_views.show_edit_nickname, name='show_edit_nickname'),
+    path('programs/<int:program_id>/my-profile/edit/submit/', teacher_views.update_nickname, name='update_nickname'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- sum_school/sum_school/settings.py
+++ sum_school/sum_school/settings.py
@@ -136,3 +136,6 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 # Set Timezone
 TIME_ZONE = 'Asia/Yangon'
 USE_TZ = True
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'sum_app/media')
