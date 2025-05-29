c2d1b0463418c730565968f7583dfea4b69f1178
zayminkhant <mzay5175@gmail.com>
2025-05-29 22:35:02 +0630

    teacher-databind and code refactor

--- sum_school/sum_app/urls/teacher_urls.py
+++ sum_school/sum_app/urls/teacher_urls.py
@@ -15,16 +15,22 @@ urlpatterns = [
     path('my-profile/update', teacher_views.teacher_userdata_update, name='updateuserdata'),
     path('my-profile/updatepassword', teacher_views.teacher_update_password, name='updatepassword'),
     
-    path('programs/<int:program_id>/dashboard/', teacher_views.program_dashboard, name='program_dashboard'),
+    path('programs/<int:program_id>/module/', teacher_views.module_redirect, name='module_redirect'),
+    path('programs/<int:program_id>/module/lesson/<str:module_code>', teacher_views.program_module, name='program_module'),
+    path('programs/<int:program_id>/module/create', teacher_views.show_tutorial_create, name='show_turorial_create'),
+    path('programs/<int:program_id>/module/<int:task_id>/details', teacher_views.program_tutorial_details, name='show_turorial_details'),
+    
     path('programs/<int:program_id>/classes/', teacher_views.program_classes, name='program_classes'),
     path('programs/<int:program_id>/classes/<int:class_id>/rolecalls/', teacher_views.show_rolecalls, name='show_rolecalls'),
+    
+    
     path('programs/<int:program_id>/activities', teacher_views.program_activities, name='program_activities'),
     path('programs/<int:program_id>/activities/create/', teacher_views.show_create_activity, name='show_create_activity'),
     path('programs/<int:program_id>/activities/create/submit/', teacher_views.create_activity, name='create_activity'),
     
     path('programs/<int:program_id>/people', teacher_views.program_users, name='program_users'),
     
-    path('programs/<int:program_id>/assignments', teacher_views.show_assignments, name='show_assignments'),
+    path('programs/<int:program_id>/assignments/', teacher_views.show_assignments, name='show_assignments'),
     path('programs/<int:program_id>/assignments/create/', teacher_views.show_create_assignment, name='show_create_assignment'),
     path('programs/<int:program_id>/assignments/create/submit/', teacher_views.create_assignment, name='create_assignment'),
     path('programs/<int:program_id>/assignments/<int:task_id>/details/', teacher_views.show_assignment_details, name='show_assignment_details'),
